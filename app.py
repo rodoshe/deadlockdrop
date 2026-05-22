0a4af59b41a269b6af9b9221c52f477086c31867
rodoshe <subat.rodoshe22@stjohns.edu>
2026-05-21 21:16:45 -0400

    added render config

--- app.py
+++ app.py
@@ -325,3 +325,8 @@ def roadmap():
 if __name__ == '__main__':
     init_db()
     app.run(debug=True)
+    
+import os
+if __name__ == '__main__':
+    init_db()
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
